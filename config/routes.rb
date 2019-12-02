72d7619c75dd9151f917a2f6122ccb149a7e8971
captproton <rocketeer.captproton@gmail.com>
2019-12-02 08:25:38 -0800

    CHANGE: linked navigation to pages that still need to be written

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,17 @@ Rails.application.routes.draw do
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
   get '/portfolio', to: 'home#portfolio'
+  get '/contact', to: 'home#contact'
+  get '/about', to: 'home#about'
+  get '/services', to: 'home#services'
+  get '/service_detail', to: 'home#service_detail'
+  get '/website_development', to: 'home#website_development'
+  get '/graphic_design', to: 'home#graphic_design'
+  get '/digital_marketing', to: 'home#digital_marketing'
+  get '/seo_content_writing', to: 'home#seo_content_writing'
+  get '/app_development', to: 'home#app_development'
+  get '/blog', to: 'home#blog'
+  
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
     end
