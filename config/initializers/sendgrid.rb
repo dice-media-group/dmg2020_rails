54e8b3a721baa90e49a9da2a6be05b04ce6d6cd1
captproton <rocketeer.captproton@gmail.com>
2019-12-20 20:08:00 -0800

    CHANGE: encrypeted config

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-  :user_name => Rails.application.credentials.development[:sendgrid_user_name],
-  :password => Rails.application.credentials.development[:sendgrid_password],
-  :domain => Rails.application.credentials.development[:sendgrid_domain_name],
+  :user_name => Rails.application.credentials.sendgrid_user_name,
+  :password => Rails.application.credentials.sendgrid_password,
+  :domain => Rails.application.credentials.sendgrid_domain_name,
   :address => 'smtp.sendgrid.net',
   :port => 465,
   :authentication => :plain,
