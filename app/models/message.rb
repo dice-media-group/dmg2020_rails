d2eb42db740ecb50d9750a123268fe97ce4d805d
captproton <rocketeer.captproton@gmail.com>
2019-12-21 17:48:51 -0800

    TESING for failure point

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
     def send_message_to_rep
-        m = NotificationMailer.contact(self)
+        m = NotificationMailer.contact(self).deliver_now
         m.deliver_now
     end
 end
